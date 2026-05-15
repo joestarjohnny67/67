555a1cb160388d3e3dfa8e6ee76b07346876ffc9
joestarjohnny67 <steelrunnerjojo@gmail.com>
2026-05-15 12:22:57 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,24 +2,42 @@ from traceback import print_tb
 
 
 class Main:
-    def __init__(self, dinheiro = 67):
+    def __init__(self, dinheiro = 0):
         self.dinheiro = dinheiro
-    def selecao(self):
-        print("--------------------")
-        print("| SISTEMA BANCARIO |")
-        print("--------------------\n")
-        print("1.Saque")
-        print("2.Deposito")
-        print("3.Trasferência")
-        print("4.Consulta de saldo\n")
-        ecolha = int(input)
     def saque(self):
         saque = float(input(f"Você tem R${self.dinheiro}, quanto quer sacar?"))
         if self.dinheiro > saque:
             self.dinheiro -= saque
-            print(f"Você sacou {saque}, e agora tem {self.dinheiro} na conta")
+            print(f"Você sacou R${saque}, e agora tem R${self.dinheiro} na conta")
         else: print("Valor invalido")
         print("")
+    def deposito(self):
+        deposito = float(input(f"Você tem R${self.dinheiro}, quanto quer depositar?"))
+        if self.dinheiro > deposito:
+            self.dinheiro -= deposito
+            print(f"Você depositou R${deposito}, e agora tem R${self.dinheiro} na conta")
+        else:
+            print("Valor invalido")
+    def transferencia(self):
+        transferencia = float(input(f"Você tem R${self.dinheiro}, quanto quer transferir?"))
+        if self.dinheiro > transferencia:
+            self.dinheiro -= transferencia
+            pessoa = input("Pra quem você deseja transferir? ")
+            print(f"Você transferiu R${transferencia}, para {pessoa} e agora tem R${self.dinheiro} na conta")
+        else:
+            print("Valor invalido")
 
 banco = Main()
-banco.selecao()
+print("--------------------")
+print("| SISTEMA BANCÁRIO |")
+print("--------------------\n")
+print("1.Saque")
+print("2.Deposito")
+print("3.Transferência")
+print("4.Consulta de saldo")
+print("5.Sair\n")
+escolha = int(input("Qual dessas operações você deseja realizar"))
+if escolha == 1: banco.saque()
+elif escolha == 2: banco.deposito()
+elif escolha == 3: banco.transferencia()
+else: print("Sistema encerrado")
